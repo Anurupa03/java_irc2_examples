12b3e50764210fe1c5a2d71c4eab7ef035b470b1
Anurupa Dhamala <asdhamala@gmail.com>
2021-11-03 11:34:45 +0545

    Capped token updated

--- irc2-token/src/main/java/com/iconloop/score/example/IRC2CappedToken.java
+++ irc2-token/src/main/java/com/iconloop/score/example/IRC2CappedToken.java
@@ -1,11 +1,18 @@
 package com.iconloop.score.example;
 
+import com.iconloop.score.token.irc2.IRC2Basic;
+import score.Address;
 import score.Context;
+import score.annotation.External;
+
 import java.math.BigInteger;
 
-public class IRC2CappedToken extends IRC2Cap {
+public class IRC2CappedToken extends IRC2Basic {
+
+    private BigInteger cap;
     public IRC2CappedToken(String _name, String _symbol, int _decimals, BigInteger _initialSupply, BigInteger _cap) {
         super(_name, _symbol, _decimals);
+        this.cap = _cap.multiply(pow10(_decimals));
 
         // mint the initial token supply here
         Context.require(_initialSupply.compareTo(BigInteger.ZERO) >= 0);
@@ -20,4 +27,24 @@ public class IRC2CappedToken extends IRC2Cap {
         }
         return result;
     }
+
+    @External
+    public void mint(BigInteger amount){
+        Context.require(Context.getCaller().equals(Context.getOwner()),"Only owners can mint tokens");
+        Context.require(cap().compareTo(super.totalSupply().add(amount)) >=0, "cap exceeded");
+        super._mint(Context.getCaller(), amount);
+    }
+
+    @External
+    public void mintTo(Address _to, BigInteger amount){
+        Context.require(Context.getCaller().equals(Context.getOwner()),"Only owners can mint tokens");
+        Context.require(cap().compareTo(super.totalSupply().add(amount)) >=0, "cap exceeded");
+        super._mint(_to, amount);
+    }
+
+    @External(readonly = true)
+    public BigInteger cap(){
+        return this.cap;
+    }
+
 }
